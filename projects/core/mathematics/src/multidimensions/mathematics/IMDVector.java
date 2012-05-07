6f9ed1af7cca6547d03cb5fb96e0c5b17de6dc50
stellarspot <stellarspot@gmail.com>
2012-05-07 15:31:05 +0400

    Initial commit

---
+++ projects/core/mathematics/src/multidimensions/mathematics/IMDVector.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multidimensions.mathematics;
+
+/**
+ *
+ * @author stellarspot
+ */
+public interface IMDVector {
+
+    int getDim();
+    double getElem(int n);
+
+    double length();
+    double scalar(IMDVector v);
+
+    IMDVector add(IMDVector v);
+    IMDVector sub(IMDVector v);
+    IMDVector mul(double a);
+
+    ICMDVector getCVector();
+
+    boolean equals(IMDVector v, double delta);
+}

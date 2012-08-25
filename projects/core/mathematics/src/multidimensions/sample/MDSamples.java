d5d7dd46f72c8e494adc1a5042ddf4e6b4b1f035
stellarspot <stellarspot@gmail.com>
2012-08-25 20:04:10 +0400

    Update Samples

---
+++ projects/core/mathematics/src/multidimensions/sample/MDSamples.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multidimensions.sample;
+
+/**
+ *
+ * @author stellarspot
+ */
+public enum MDSamples {
+
+    SHAPE("MD Shapes", MDShapeSamples.values());
+    String category;
+    IMDSample[] samples;
+
+    private MDSamples(String category, IMDSample[] samples) {
+        this.category = category;
+        this.samples = samples;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public IMDSample[] getSamples() {
+        return samples;
+    }
+}
